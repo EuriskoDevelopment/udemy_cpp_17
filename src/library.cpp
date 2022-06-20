e0c280bc50c76e1ccb6cbac4db10aa4e652c1003
EuriskoDeveloment <re@pandema.com>
2022-06-20 20:22:29 +0200

    Finnish project 2

--- src/library.cpp
+++ src/library.cpp
@@ -8,11 +8,62 @@ using namespace std;
 struct User {
     int id;
     string name;
+    int borrowedBookIds[MAX_BOOKS];
+    int nbrBorrowedBooks;
 
     User()
     {
         id = 0;
         name = "";
+        nbrBorrowedBooks = 0;
+    }
+
+    void borrowBook(int bookId)
+    {
+        borrowedBookIds[nbrBorrowedBooks++] = bookId;
+    }
+
+    void returnBook(int bookId)
+    {
+
+        int* index = find(borrowedBookIds, borrowedBookIds + nbrBorrowedBooks, bookId) - nbrBorrowedBooks;
+        cout << "Returning book id: " << bookId << " index: " << *index << endl;
+
+        if (*index >= nbrBorrowedBooks) {
+            cout << "Not a borrowed book" << endl;
+            return;
+        }
+        for (int i = *index; i <= nbrBorrowedBooks; i++) {
+            borrowedBookIds[i] = borrowedBookIds[i + 1];
+        }
+
+        nbrBorrowedBooks--;
+    }
+
+    bool is_borrowed(int book_id)
+    {
+        for (int i = 0; i < nbrBorrowedBooks; ++i) {
+            if (book_id == borrowedBookIds[i])
+                return true;
+        }
+        return false;
+    }
+
+    void read()
+    {
+        cout << "Enter user name and id: ";
+        cin >> name >> id;
+    }
+
+    void print()
+    {
+        // sort what he borrowed
+        sort(borrowedBookIds, borrowedBookIds + nbrBorrowedBooks);
+
+        cout << "user " << name << " id " << id << " borrowed books ids: ";
+        for (int i = 0; i < nbrBorrowedBooks; ++i)
+            cout << borrowedBookIds[i] << " ";
+        cout << "\n";
     }
 };
 
@@ -26,6 +77,7 @@ struct Book {
     {
         cout << "Enter book id, title & quantity: ";
         cin >> id >> name >> quantity;
+        nbrOfBorrowed = 0;
     }
 
     void print()
@@ -33,6 +85,11 @@ struct Book {
         cout << "id = " << id << " name = " << name << " quantity "
              << quantity << " total_borrowed " << nbrOfBorrowed
              << "\n";
+    }
+
+    bool operator()(Book& a) const
+    {
+        return a.id == id;
     }
 };
 
@@ -44,6 +101,11 @@ bool compareById(Book& a, Book& b)
 bool compareByName(Book& a, Book& b)
 {
     return a.name < b.name;
+}
+
+const int isSameBook(Book& a, Book& b)
+{
+    return a.name == b.name;
 }
 
 struct LibrarySystem {
@@ -78,6 +140,96 @@ struct LibrarySystem {
         }
     }
 
+    void add_user()
+    {
+        userList[nbrOfUsers++].read();
+    }
+
+    int find_book_idx_by_name(string name)
+    {
+        for (int i = 0; i < nbrOfBooks; ++i) {
+            if (name == bookList[i].name)
+                return i;
+        }
+        return -1;
+    }
+
+    void user_borrow_book()
+    {
+        string name;
+        int bookId;
+        cout << "Enter name and book id to borrow: ";
+        cin >> name >> bookId;
+
+        // Use lambda function here.
+        //[id = bookId](const Book& book) -> bool {return book.id == id};
+        //int i = find(bookList, bookList + nbrOfBooks, [id = bookId](const Book& book) -> bool {return book.id == id;});
+
+        for (auto& book : bookList) {
+            if (book.id == bookId) {
+                book.nbrOfBorrowed++;
+            }
+        }
+
+        for (auto& user : userList) {
+            if (user.name == name) {
+                user.borrowBook(bookId);
+            }
+        }
+    }
+
+    void user_return_book()
+    {
+        string name;
+        int bookId;
+        cout << "Enter name and book id to return: ";
+        cin >> name >> bookId;
+
+        for (auto& book : bookList) {
+            if (book.id == bookId) {
+                book.nbrOfBorrowed--;
+            }
+        }
+
+        for (auto& user : userList) {
+            if (user.name == name) {
+                user.returnBook(bookId);
+            }
+        }
+    }
+
+    void print_users()
+    {
+        cout << "\n";
+        for (int i = 0; i < nbrOfUsers; ++i)
+            userList[i].print();
+    }
+
+    void print_who_borrowed_book_by_name()
+    {
+        string book_name;
+        cout << "Enter book name: ";
+        cin >> book_name;
+
+        int book_idx = find_book_idx_by_name(book_name);
+
+        if (book_idx == -1) {
+            cout << "Invalid book name.\n";
+            return;
+        }
+        int book_id = bookList[book_idx].id;
+
+        if (bookList[book_idx].nbrOfBorrowed == 0) {
+            cout << "No borrowed copies\n";
+            return;
+        }
+
+        for (int i = 0; i < nbrOfUsers; ++i) {
+            if (userList[i].is_borrowed(book_id))
+                cout << userList[i].name << "\n";
+        }
+    }
+
     void run()
     {
         while (true) {
@@ -87,20 +239,20 @@ struct LibrarySystem {
                 add_book();
             //else if (choice == 2)
             //search_books_by_prefix();
-            //else if (choice == 3)
-            //print_who_borrowed_book_by_name();
+            else if (choice == 3)
+                print_who_borrowed_book_by_name();
             else if (choice == 4)
                 print_library_by_id();
             else if (choice == 5)
                 print_library_by_name();
-            //else if (choice == 6)
-            //add_user();
-            //else if (choice == 7)
-            //user_borrow_book();
-            //else if (choice == 8)
-            //user_return_book();
-            //else if (choice == 9)
-            //print_users();
+            else if (choice == 6)
+                add_user();
+            else if (choice == 7)
+                user_borrow_book();
+            else if (choice == 8)
+                user_return_book();
+            else if (choice == 9)
+                print_users();
             else
                 break;
         }
